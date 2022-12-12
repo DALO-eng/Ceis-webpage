0b91fb0258ebadd1e9355486f45e474d7ecc1172
Daniel Alejandro Leon Ortiz <dalobusiness01@gmail.com>
2022-12-11 19:18:28 -0500

    Added first version of header and footer

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -3,15 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    HomePageComponent
-  ],
-  imports: [
-    CommonModule,
-    LandingPageRoutingModule
-  ]
+  declarations: [HomePageComponent, HeaderComponent, FooterComponent],
+  imports: [CommonModule, LandingPageRoutingModule, FontAwesomeModule],
 })
-export class LandingPageModule { }
+export class LandingPageModule {}
